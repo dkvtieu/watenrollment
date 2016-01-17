5800bb5e13cb18851d2d1cb7fc783e1080b36096
dkvtieu <dkhac.tieu@gmail.com>
2016-01-17 00:03:20 -0500

    Removal of unnecessary code
    - Cleaned up code in global scope
    - Minor improvements to documentation

--- enroll-checker.py
+++ enroll-checker.py
@@ -1,60 +1,42 @@
 from lxml import html, etree
 import time
 import urllib2
-import smtplib
-import winsound
 
+
+###USER INPUT###
 term = raw_input('Term? (e.g. 1161 is Winter 2016) ')
 subject = raw_input('Subject? (e.g. MATH) ')
 catalog = raw_input('Course Number? (e.g. 240) ')
 section_type = raw_input('Are you looking for a LEC or TUT? (e.g. LEC) ')
 section_number = raw_input('Section? (e.g. 001) ')
-ask_email = raw_input('Would you like to recieve email notifications when a spot opens up? [y/n] ')
-
-
-if ask_email is 'y':
-	your_email = raw_input('Please provide an email for notification ')	
-else:
-	True
+section_number = int(section_number)
 refresh_rate = raw_input('Refresh Rate? ')
 refresh_rate = int(refresh_rate)
 
-url = 'http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=%s&subject=%s&cournum=%s' % (term, subject.upper(), catalog)
-
-"""
-# EMAIL HANDLING #
-msg='hello'
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-server.login("EMAIL", "PASS")
-server.sendmail('from_email', 'your_email', msg)
-server.quit()
-"""
-
-#column index for the table of information
+#column index for table of information
 comp_sec = 2
 enrl_cap = 6
 enrl_tot = 7
-instructor = 12
 
 
+#queries the url and feeds it to the lxml parser for further use
 def get_page():
+	url = 'http://www.adm.uwaterloo.ca/cgi-bin/cgiwrap/infocour/salook.pl?level=under&sess=%s&subject=%s&cournum=%s' % (term, subject.upper(), catalog)
 	request = urllib2.Request(url)
 	page = urllib2.urlopen(request)
 	html_parser = etree.HTMLParser()
 	tree = etree.parse(page, html_parser)
 	return tree
 
-number_of_rows = get_page().xpath('//table[@border="2"]//table/tr')
-
+#gets all the text elements for the row in question
 def get_row_info(row_index):
 	display_row = get_page().xpath('//table[@border="2"]//table/tr[%d]//td/text()' % row_index)
 	return display_row
 
-#creates a list of lists where each element is a valid row in the data table
+#creates a list of lists where each element is a valid row (with its text elements) in the data table
 def row_list():
 	list = []
+	number_of_rows = get_page().xpath('//table[@border="2"]//table/tr')
 	for i in range(2, len(number_of_rows)): #begins at 2 so that it disregards the first two useless rows
 		list.append(get_row_info(i))
 	return list
@@ -74,11 +56,11 @@ def main():
 		enrollment_capacity = int(correct_row[enrl_cap])
 		enrollment_total = int(correct_row[enrl_tot])
 		if enrollment_total >= enrollment_capacity: 
-			print 'All %d seats are unavailable for %s %03d of %s %s' % (enrollment_capacity, section_type.upper(), int(section_number), subject.upper(), catalog)
+			print 'All %d seats are unavailable for %s %03d of %s %s' % (enrollment_capacity, section_type.upper(), section_number, subject.upper(), catalog)
 			time.sleep(refresh_rate)
 		else:
 			seats_available = enrollment_capacity - enrollment_total
-			print "There are now %d seat(s) available for %s %03d of %s %s" % (seats_available, section_type.upper(), int(section_number), subject.upper(), catalog)
+			print "There are now %d seat(s) available for %s %03d of %s %s" % (seats_available, section_type.upper(), section_number, subject.upper(), catalog)
 			time.sleep(15) #makes sure the message stays visible to the user for a brief period of time 
 			break
 
